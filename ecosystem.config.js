7cec7bb7cd27ac18f47b45304e65822c4b880f94
seongto <smalllife@naver.com>
2020-05-25 16:24:48 +0900

    fix pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,19 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    name: 'allius-svelte',
+    script: 'npm run start-web',
+    watch: true,
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
   }],
 
   deploy : {
     production : {
-      user : 'SSH_USERNAME',
+      usster : 'SSH_USERNAME',
       host : 'SSH_HOSTMACHINE',
       ref  : 'origin/master',
       repo : 'GIT_REPOSITORY',
