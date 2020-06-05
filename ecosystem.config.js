fb5ca99e2fbae51d4e2bd34f7a5f38d6cd541f12
seongto <smalllife@naver.com>
2020-06-05 20:20:00 +0900

    run public 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'allius-svelte',
-    script: 'npm run start-web',
+    script: 'npm run start-public',
     watch: true,
     env: {
       NODE_ENV: "development",
