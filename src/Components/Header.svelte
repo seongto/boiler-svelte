4294af69e3d3f92ee6631041097aa6fff92b9e33
seongto <smalllife@naver.com>
2020-06-16 12:55:21 +0900

    http 리다이렉션에 개발서버 도메인 제외 포함

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -12,8 +12,10 @@
     onMount(() => {
         let addrs = location.href.substring(location.protocol.length);
 
-        if ((location.protocol !== 'https:') && (addrs !== '//localhost:5000/')) {
-            location.replace(`https:${location.href.substring(location.protocol.length)}`);
+        if (location.protocol !== 'https:') {
+            if ((addrs !== '//localhost:5000/') && (addrs !== '//dev0828rw.allius.io/')) {
+                location.replace(`https:${location.href.substring(location.protocol.length)}`);
+            }
         }
 
         loginCheck = Boolean(getCookie('allius_id'));
